56ce06a3407e96774544e9e023729216fc7acaf6
GreenCode Dev <52554129+greencodedev@users.noreply.github.com>
2021-02-13 00:07:43 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -9,7 +9,7 @@ export async function getDataFromAPI(uri) {
         host + uri,
         {
             headers: { 
-                "Authorization": "Bearer d4a1b13447a21df247a971101e57fc3212970dd1"
+                "Authorization": "Bearer [your-github-key]"
             },
         }
     ).then(response => {
